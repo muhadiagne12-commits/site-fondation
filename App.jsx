7448e69b107bc9b9abb8b6a7e7858f63343ab29a
muhadiagne12-commits <muhadiagne12@gmail.com>
2026-05-20 15:14:50 +0000

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,6 @@
+# Dans ton terminal
+npm create vite@latest mon-site-fondation -- --template react
+cd mon-site-fondation
+npm install
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
